79856b83e15763cd1f02f298134e6bdf5779c312
rotarymars <s.goto2050@gmail.com>
2024-12-11 22:21:27 +0900

    formatted main.py

--- main.py
+++ main.py
@@ -31,77 +31,90 @@ BEFLNUM = 0
 BEFRNUM = 0
 cnt = 0
 
+
 class LINE:
-  def __init__(self, vertical, horizontal):
-    self.target_url = f"http://roboberry.local:81/techno_cam/line_colors?latlx=1008&latly=0&labrx=3608&labry=2592&bc_h={str(horizontal)}&bc_v={str(vertical)}&bg_h=50&bg_v=50"
-  def getdata(self):
-    try:
-      json_data = urequests.get(self.target_url).json
-    except:
-      return None
-    return json_data["colors"]
+
+    def __init__(self, vertical, horizontal):
+        self.target_url = f"http://roboberry.local:81/techno_cam/line_colors?latlx=1008&latly=0&labrx=3608&labry=2592&bc_h={str(horizontal)}&bc_v={str(vertical)}&bg_h=50&bg_v=50"
+
+    def getdata(self):
+        try:
+            json_data = urequests.get(self.target_url).json
+        except:
+            return None
+        return json_data["colors"]
+
 
 class RESCUE_OBJ_DETECTION:
-  def __init__(self):
-    self.target_url = "http://roboberry.local:81/techno_cam/rescue_objects"
-  def getdata(self):
-    try:
-      json_data = urequests.get(self.target_url).json
-    except:
-      return None
-    return json_data["objects"]
+
+    def __init__(self):
+        self.target_url = "http://roboberry.local:81/techno_cam/rescue_objects"
+
+    def getdata(self):
+        try:
+            json_data = urequests.get(self.target_url).json
+        except:
+            return None
+        return json_data["objects"]
+
 
 def isgreenhue(hue: int):
-  return 150<=hue<=210
+    return 150 <= hue <= 210
 
-LINE_TRACE_SENSOR = LINE(3,3)
+
+LINE_TRACE_SENSOR = LINE(3, 3)
 RESCUE_OBJECT_DETECTION_SENSOR = RESCUE_OBJ_DETECTION()
 while True:
-  cnt+=1
-  if Button.CENTER in EV3.buttons.pressed():
-    MOTORL.brake()
-    MOTORR.brake()
-    EV3.speaker.beep()
-    time.sleep(0.5)
-    while True:
-      if Button.CENTER in EV3.buttons.pressed():
+    cnt += 1
+    if Button.CENTER in EV3.buttons.pressed():
+        MOTORL.brake()
+        MOTORR.brake()
         EV3.speaker.beep()
         time.sleep(0.5)
-        break
-  if now := LINE_TRACE_SENSOR.getdata() is None:
-    continue
-  else:
-    ISUM=now[1][0]-now[7][0]
-    DSUM=(BEFLNUM-BEFRNUM)-(now[1][0]-now[7][0])
-    MOTORL.run(DEFAULTSPEED + DEFAULTPROPORTION * (now[1][0]-now[7][0])+DEFAULTI*ACCUMI+DEFAULTD*ACCUMD)
-    MOTORR.run(DEFAULTSPEED + DEFAULTPROPORTION * (now[7][0]-now[1][0])-DEFAULTI*ACCUMI-DEFAULTD*ACCUMD)
-    BEFLNUM=now[1][0]
-    BEFRNUM=now[7][0]
-  if cnt>=20 and BLACKTHRESHOLD>=now[1][0] and not BLACKTHRESHOLD>=now[7][0]:
-    EV3.speaker.beep()
-    while BLACKTHRESHOLD>=now[1][0]:
-      now=LINE_TRACE_SENSOR.getdata()
-      MOTORL.run(200)
-      MOTORR.run(200)
-    while not BLACKTHRESHOLD>=now[7][0]:
-      now=LINE_TRACE_SENSOR.getdata()
-      MOTORL.run(-200)
-      MOTORR.run(200)
-    MOTORL.run(200)
-    MOTORR.run(-200)
-    time.sleep(0.3)
-    cnt = 0
-  if cnt>=20 and BLACKTHRESHOLD>=now[7][0] and not BLACKTHRESHOLD>=[1][0]:
-    EV3.speaker.beep()
-    while BLACKTHRESHOLD>=[7][0]:
-      now=LINE_TRACE_SENSOR.getdata()
-      MOTORL.run(200)
-      MOTORR.run(200)
-    while not BLACKTHRESHOLD>=now[1][0]:
-      now=LINE_TRACE_SENSOR.getdata()
-      MOTORL.run(200)
-      MOTORR.run(-200)
-    MOTORL.run(-200)
-    MOTORR.run(200)
-    time.sleep(0.3)
-    cnt=0
+        while True:
+            if Button.CENTER in EV3.buttons.pressed():
+                EV3.speaker.beep()
+                time.sleep(0.5)
+                break
+    if now := LINE_TRACE_SENSOR.getdata() is None:
+        continue
+    else:
+        ISUM = now[1][0] - now[7][0]
+        DSUM = (BEFLNUM - BEFRNUM) - (now[1][0] - now[7][0])
+        MOTORL.run(DEFAULTSPEED + DEFAULTPROPORTION * (now[1][0] - now[7][0]) +
+                   DEFAULTI * ACCUMI + DEFAULTD * ACCUMD)
+        MOTORR.run(DEFAULTSPEED + DEFAULTPROPORTION * (now[7][0] - now[1][0]) -
+                   DEFAULTI * ACCUMI - DEFAULTD * ACCUMD)
+        BEFLNUM = now[1][0]
+        BEFRNUM = now[7][0]
+    if cnt >= 20 and BLACKTHRESHOLD >= now[1][
+            0] and not BLACKTHRESHOLD >= now[7][0]:
+        EV3.speaker.beep()
+        while BLACKTHRESHOLD >= now[1][0]:
+            now = LINE_TRACE_SENSOR.getdata()
+            MOTORL.run(200)
+            MOTORR.run(200)
+        while not BLACKTHRESHOLD >= now[7][0]:
+            now = LINE_TRACE_SENSOR.getdata()
+            MOTORL.run(-200)
+            MOTORR.run(200)
+        MOTORL.run(200)
+        MOTORR.run(-200)
+        time.sleep(0.3)
+        cnt = 0
+    if cnt >= 20 and BLACKTHRESHOLD >= now[7][0] and not BLACKTHRESHOLD >= [
+            1
+    ][0]:
+        EV3.speaker.beep()
+        while BLACKTHRESHOLD >= [7][0]:
+            now = LINE_TRACE_SENSOR.getdata()
+            MOTORL.run(200)
+            MOTORR.run(200)
+        while not BLACKTHRESHOLD >= now[1][0]:
+            now = LINE_TRACE_SENSOR.getdata()
+            MOTORL.run(200)
+            MOTORR.run(-200)
+        MOTORL.run(-200)
+        MOTORR.run(200)
+        time.sleep(0.3)
+        cnt = 0
