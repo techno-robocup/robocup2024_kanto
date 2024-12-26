98d912ee124a3d8d1241d084827bfc0c252fbe6a
rotarymars <s.goto2050@gmail.com>
2024-12-27 00:30:32 +0900

    changed many stuff

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from techno_client.techno_client.techno_client import TechnoClient
 import urequests
 import socket
 import time
+
+
+while True:
+    pass
 
 EV3 = EV3Brick()
 MOTORL = Motor(Port.C)
--- send.sh
+++ send.sh
@@ -1,1 +1,3 @@
-scp main.py robot@ev3dev.local:
+ssh ev3dev.local "rm -rf ~/robocup"
+ssh ev3dev.local "mkdir ~/robocup"
+scp -Cr . robot@ev3dev.local:~/robocup
